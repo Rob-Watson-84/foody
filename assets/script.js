dea29a1c0ee26eaf365c2206f2ba64e60d1ed9d4
Anthony Slaviero <tonyslaviero@gmail.com>
2022-08-08 21:21:56 -0700

    javescriptupdate

--- assets/script.js
+++ assets/script.js
@@ -13,47 +13,63 @@ $("#submit-btn").on("click", function (event) {
   var veggies = $("#veggies").val().trim();
   // var other = $("#other").val().trim();
 
-  var urlTag = protein + "+" + carbs + "+" + veggies; 
+  var urlTag = protein + "+" + carbs + "+" + veggies;
   var spoonUrl = "https://api.spoonacular.com/recipes/complexSearch?apiKey=" + spoonAPI + "&query=" + urlTag
+  var mealDbUrl = "https://www.themealdb.com/api/json/v1/1/filter.php?i=" + protein
   console.log(spoonUrl);
-    fetch(spoonUrl)
+  console.log(mealDbUrl);
+
+  fetch(spoonUrl)
     .then((response) => response.json())
     .then(data => {
       console.log(data);
-      recipeSection(data)
+      var recipeID = data.results[0].id
+      console.log(`Recipe ID: ${recipeID}`)
     })
     .catch(error => {
-      console.log("Error");
+      console.log(error);
+    });
+
+    fetch(mealDbUrl)
+    .then((response) => response.json())
+    .then(data => {
+      console.log(data);
+      var imageOne = document.createElement("img");
+      document.getElementById("foodpics").appendChild(imageOne);
+      imageOne.setAttribute("src", data.meals[0].strMealThumb);
+    })
+    .catch(error => {
+      console.log(error);
     });
 });
 
 
 //create list of recipe names and pictures
-function recipeSection(data) {
-  recipeDiv.innerHTML = "";
-  if (data.results.length > 0) {
-    totalData = data.results;
-  for(let i = 0; i < totalData.length; i++){
-    console.log(totalData);
-    let img = document.createElement("img");
-    let div = document.createElement('div');
-    div.setAttribute("class", "col-sm-3"); //needs bulma properties added
-    div.innerHTML += '<h3><a href="javascript:void(0);" onclick="goToSourceURL('+totalData[i].id +')">' + totalData[i].title + '</a></h3>'
-    div.innerHTML += '<img class="img-fluid" src="'+totalData[i].image+'">'  
-   recipeDiv.appendChild(div);
-  } 
-  }else {recipeDiv.innerHTML = "Unable to locate results. Please try your search again"}
-  }
 
-function recipe(recipeID){
+function recipe(recipeID) {
   var idUrl = `https://api.spoonacular.com/recipes/${recipeID}/information?includeNutrition=false?apiKey=` + spoonAPI
   console.log(idUrl)
   fetch(idUrl)
     .then((response) => response.json())
     .then(data => {
-      recipeSection(data);
+      // recipeSection(data);
     })
     .catch(error => {
       console.log("Error");
     });
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
